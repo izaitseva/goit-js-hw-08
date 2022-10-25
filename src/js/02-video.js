f33b9ae7a67afeb9f9f592ec97f314f663cab00d
Iryna Zaitseva <zaitseva02@gmail.com>
2022-10-25 23:28:07 +0300

    02 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const curTimeKey = "videoplayer-current-time";
+
+player.on('timeupdate', function({seconds}) {
+    localStorage.setItem(curTimeKey, seconds);
+});
+
+function initCurTime() {
+    let secondsStr = localStorage.getItem(curTimeKey);
+    let seconds = Number(secondsStr);
+
+    player.setCurrentTime(seconds).then();
+}
+
+initCurTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+ const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.message'),
+}
 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextAreaInput);
+
+function onTextAreaInput(event) {
+    const message = event.currentTarget.message;
+
+    localStorage.setItem('feedback-msg', message);
+}
