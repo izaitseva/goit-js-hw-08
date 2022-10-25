29e89679e8c1515e64bfe59a62c12d58dcfc7eb6
Iryna Zaitseva <zaitseva02@gmail.com>
2022-10-26 00:09:42 +0300

    hw-task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const curTimeKey = "videoplayer-current-time";
 
-player.on('timeupdate', function({seconds}) {
+player.on('timeupdate', throttle(function({seconds}) {
+    console.log(1000);
     localStorage.setItem(curTimeKey, seconds);
-});
+}, 1000));
+
 
 function initCurTime() {
     let secondsStr = localStorage.getItem(curTimeKey);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,19 @@
- const refs = {
+const refs = {
     form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('.message'),
+    textarea: document.querySelector('[name=message]'),
 }
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', onTextAreaInput);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log("submit");
+}
+
 function onTextAreaInput(event) {
-    const message = event.currentTarget.message;
+    const message = event.currentTarget.value;
 
     localStorage.setItem('feedback-msg', message);
 }
